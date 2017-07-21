d62946373bf8bde9d06a1d4991b3512778312e6b
Po Chun, Lu <oceanus11034@gmail.com>
2017-07-21 22:57:03 +0800

    [Add] day22

---
+++ day22-closures.js
@@ -0,0 +1,53 @@
+// from https://github.com/mqyqingfeng/Blog/issues/10
+
+var scope = "global scope";
+function checkscope(){
+    var scope = "local scope";
+    function f(){
+        return scope;
+    }
+    return f;
+}
+
+var foo = checkscope();
+foo(); // local
+
+// --
+
+
+var data = [];
+
+for (var i = 0; i < 3; i++) {
+  data[i] = (function (i) {
+        return function(){
+            console.log(i);
+        }
+  })(i);
+}
+
+data[0](); // 0
+data[1](); // 1
+data[2](); // 2
+
+//
+
+var obj = {
+    value: 1
+};
+function foo(o) {
+    o.value = 2;
+    console.log(o.value); //2
+}
+foo(obj);
+console.log(obj.value) // 2
+
+var obj = {
+    value: 1
+};
+function foo(o) {
+    o = 2;
+    console.log(o); //2
+}
+foo(obj);
+console.log(obj.value) // 1
+
