f6ac31bf483710db77e94349deb659048c1a4b95
Po Chun, Lu <oceanus11034@gmail.com>
2017-07-29 23:51:30 +0800

    [Add] day28

---
+++ day28-let.js
@@ -0,0 +1,37 @@
+
+var liList = document.querySelectorAll('li')
+for( let i=0; i<liList.length; i++){
+  liList[i].onclick = function(){
+    console.log(i) // 0, 1, 2, 3, 4, 5
+  }
+}
+
+var liList = document.querySelectorAll('li')
+for( let i=0; i<liList.length; i++){
+  let i = i
+  liList[i].onclick = function(){
+    console.log(i)
+  }
+}
+// 
+x = "global";
+// function scope:
+(function() {
+    x; // not "global"
+
+    var/let/… x;
+}());
+// block scope (not for `var`s):
+{
+    x; // not "global"
+
+    let/const/… x;
+}
+
+// let tdz
+let x = 'global'
+{
+  console.log(x) // Uncaught ReferenceError: x is not defined
+  let x = 1
+}
+
